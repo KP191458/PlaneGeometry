146e09bae5fbffac79dac4e2ecdf9955184e04f1
Konrad Pławik <kp@Konrads-iMac.local>
2019-11-18 21:57:06 +0100

    Point defined.

---
+++ PlaneGeometry.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+struct Punkt
+{
+    Punkt(int x, int y)
+        :x(x), y(y)
+        {}
+
+    int x;
+    int y;
+};
+
+
+int main ()
+{
+    cout << "Geometria:" << endl;
+    return 0;
+}
