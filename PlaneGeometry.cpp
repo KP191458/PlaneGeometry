1d7e791e2041f1cb32337bb0e07702e85780b010
Konrad Pławik <kp@Konrads-iMac.local>
2019-11-22 23:24:37 +0100

    Dodano wysrodkowanie oraz wyswietlanie punktu i figury.

--- PlaneGeometry.cpp
+++ PlaneGeometry.cpp
@@ -22,6 +22,42 @@ struct Figura
     vector<Punkt> punkty;
 };
 
+Punkt Dodaj( Punkt a, Punkt b )
+{
+    return Punkt(a.x+b.x, a.y+b.y);
+};
+
+void wysrodkuj(Figura &fig)
+{
+    for(Punkt p : fig.punkty)
+    {
+        p = Dodaj(fig.srodek, p);
+    }
+};
+
+void wyswietlPunkt(Punkt p)
+{
+    cout << "(" << p.x;
+    cout << p.y << ")";
+    cout << endl;
+};
+
+void podajInfo(Figura fig)
+{
+    cout << "Figura:" << endl;
+    cout << "Srodek : ";
+    wyswietlPunkt(fig.srodek);
+    cout << endl;
+    cout << "Punkty :" << endl;    
+    int i = 0;
+    for(Punkt p : fig.punkty)
+    {
+        i++;
+        cout << i << " ";
+        wyswietlPunkt(p);
+    }
+};
+
 int main ()
 {
     cout << "Geometria:" << endl;
